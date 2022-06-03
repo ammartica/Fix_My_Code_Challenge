2ad2675a433aea13d68e0c12ff862291e889068b
ammartica <amisadaymartinez@gmail.com>
2022-06-03 15:04:16 +0000

    changed perimeter formula

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,7 +18,7 @@ class Square():
 
     def PermiterOfMySquare(self):
         """ Returns perimeter of Square object """
-        return (self.width * 2) + (self.height * 2)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         """ String representation of Square object """
